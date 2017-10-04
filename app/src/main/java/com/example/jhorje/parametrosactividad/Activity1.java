22aec2df48de47708dabdc0453c0d1eca055365e
Jorge <wiijlg@hotmail.com>
2017-10-04 13:04:11 +0200

    Pantalla Principal muestra edad despues de indicarla

--- app/src/main/java/com/example/jhorje/parametrosactividad/Activity1.java
+++ app/src/main/java/com/example/jhorje/parametrosactividad/Activity1.java
@@ -4,10 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.content.Intent;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class Activity1 extends AppCompatActivity implements View.OnClickListener {
@@ -15,22 +17,57 @@ public class Activity1 extends AppCompatActivity implements View.OnClickListener
     private Button btnEnviar;
     private EditText txtName;
     private RadioButton rdBtnHom, rdBtnMuj;
+    private TextView txtEdad;
+    private LinearLayout layDatos, layEdad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
 
+        //Asociar elementos Vista <-> Controlador
         btnEnviar = (Button) findViewById(R.id.btnEnviar);
         txtName = (EditText) findViewById( R.id.txtName );
         rdBtnHom = (RadioButton) findViewById( R.id.radioButton2) ;
         rdBtnMuj = (RadioButton) findViewById( R.id.radioButton) ;
+        txtEdad = (TextView) findViewById(R.id.txtEdad);
+        layDatos = (LinearLayout) findViewById( R.id.linearDatos) ;
+        layEdad = (LinearLayout) findViewById( R.id.linearEdad) ;
+
+        //Obtener valores pantalla 2
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if (extras != null) {//ver si contiene datos
+            int datoEdad = (int) extras.get("edad");//Obtengo la edad
+
+            String mensajeGuay = "";
+            if (datoEdad < 35){
+                if (datoEdad >= 25){
+                    mensajeGuay = "estas en la flor de la vida";
+                } else {
+                    if ( datoEdad >= 18){
+                        mensajeGuay = "ja eres major d'edat";
+                    } else {
+                        mensajeGuay = "sal cuanto antes, que no se enteren de que has usado esta app";
+                    }
+                }
+            } else {
+                mensajeGuay = "ai, ai, ai...";
+            }
+
+            txtEdad.setText("Tienes unos " + datoEdad + " años, " + mensajeGuay);
+            layEdad.setVisibility(View.VISIBLE);
+            layDatos.setVisibility(View.INVISIBLE);
+        } else {
+            //No se ha recibido datos
+            layEdad.setVisibility(View.INVISIBLE);
+        }
 
         btnEnviar.setOnClickListener(this);
     }
 
 
-
+    //Comprobador radiobuttons checkeados
     public void onRadioButtonClicked(View view) {
 
         //Boton pulsado?
@@ -47,6 +84,7 @@ public class Activity1 extends AppCompatActivity implements View.OnClickListener
         }
     }
 
+    //Clic en botón
     public void onClick(View v) {
 
         Log.w("BETA","Has pinchado");
--- app/src/main/java/com/example/jhorje/parametrosactividad/Activity2.java
+++ app/src/main/java/com/example/jhorje/parametrosactividad/Activity2.java
@@ -1,13 +1,20 @@
 package com.example.jhorje.parametrosactividad;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class Activity2 extends AppCompatActivity {
+public class Activity2 extends AppCompatActivity implements View.OnClickListener {
 
     TextView txtPantalla;
+    private EditText txtEdad;
+    private Button btnContinuar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +22,8 @@ public class Activity2 extends AppCompatActivity {
         setContentView(R.layout.activity_2);
 
         txtPantalla = (TextView) findViewById( R.id.textView );
-
+        txtEdad = (EditText) findViewById(R.id.editText);
+        btnContinuar = (Button) findViewById(R.id.button);
 
         Intent intent=getIntent();
         Bundle extras =intent.getExtras();
@@ -25,5 +33,22 @@ public class Activity2 extends AppCompatActivity {
 
             txtPantalla.setText("Hola " +  datoNombre + ", eres " + datoSexo);
         }
+
+        btnContinuar.setOnClickListener(this);
+    }
+
+    //Clic en botón
+    public void onClick(View v) {
+
+        Intent explicit_intent;//Declaro el Intent
+
+        //Creando Intent
+        explicit_intent = new Intent(this,Activity1.class);
+        int auxEditEdad = Integer.parseInt(txtEdad.getText().toString());
+
+        explicit_intent.putExtra("edad",auxEditEdad);//Guardamos la edad
+
+        //lo iniciamos pasandole la intencion, con todos sus parametros guardados
+        startActivity(explicit_intent);
     }
 }
